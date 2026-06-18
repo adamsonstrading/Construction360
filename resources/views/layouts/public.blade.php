4004388c85c039ad4b058cd13a740b814fb37030
HumayounAkhtar <humayounakhtar.ha@gmail.com>
2026-06-18 11:21:06 +0100

    Update social icons to original brand designs

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -230,18 +230,32 @@
                         </p>
 
                         <!-- Social Media Links -->
-                        <div class="flex flex-wrap items-center gap-x-5 gap-y-2 text-xs font-bold text-slate-500 font-sans tracking-wide pt-2">
-                            <a href="{{ $content['social_facebook'] ?? 'https://www.facebook.com/people/Construction-360/61590797767639/' }}" class="hover:text-aqua flex items-center space-x-1.5 transition-colors">
-                                <svg class="h-3.5 w-3.5" fill="currentColor" viewBox="0 0 24 24"><path d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H8v-3h2V9.5C10 7.57 11.57 6 13.5 6H16v3h-2c-.55 0-1 .45-1 1V12h3v3h-3v6.8c4.56-.93 8-4.96 8-9.8z"/></svg>
-                                <span>Facebook</span>
+                        <div class="flex flex-wrap items-center gap-x-5 gap-y-2 pt-2">
+                            <a href="{{ $content['social_facebook'] ?? 'https://www.facebook.com/people/Construction-360/61590797767639/' }}" target="_blank" aria-label="Facebook" class="hover:opacity-80 transition-opacity">
+                                <!-- Original Facebook Icon -->
+                                <svg class="h-6 w-6" fill="#1877F2" viewBox="0 0 24 24">
+                                    <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.469h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.469h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/>
+                                </svg>
                             </a>
-                            <a href="{{ $content['social_instagram'] ?? 'https://www.instagram.com/Construction360.co' }}" class="hover:text-aqua flex items-center space-x-1.5 transition-colors">
-                                <svg class="h-3.5 w-3.5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M3 9a6 6 0 016-6h6a6 6 0 016 6v6a6 6 0 01-6 6H9a6 6 0 01-6-6V9z"/><path stroke-linecap="round" stroke-linejoin="round" d="M12 15a3 3 0 100-6 3 3 0 000 6zM16.5 7.5h.01"/></svg>
-                                <span>Instagram</span>
+                            <a href="{{ $content['social_instagram'] ?? 'https://www.instagram.com/Construction360.co' }}" target="_blank" aria-label="Instagram" class="hover:opacity-80 transition-opacity">
+                                <!-- Original Instagram Icon -->
+                                <svg class="h-6 w-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <defs>
+                                        <linearGradient id="ig-gradient" x1="12" y1="24" x2="12" y2="0" gradientUnits="userSpaceOnUse">
+                                            <stop stop-color="#FEC564"/>
+                                            <stop offset="0.328" stop-color="#FF5252"/>
+                                            <stop offset="0.646" stop-color="#C21975"/>
+                                            <stop offset="1" stop-color="#4C5FD7"/>
+                                        </linearGradient>
+                                    </defs>
+                                    <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zM12 0C8.741 0 8.333.014 7.053.072 2.695.272.273 2.69.073 7.052.014 8.333 0 8.741 0 12c0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98C8.333 23.986 8.741 24 12 24c3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98C15.668.014 15.259 0 12 0zm0 5.838a6.162 6.162 0 100 12.324 6.162 6.162 0 000-12.324zM12 16a4 4 0 110-8 4 4 0 010 8zm3.975-11.413a1.44 1.44 0 100 2.88 1.44 1.44 0 000-2.88z" fill="url(#ig-gradient)"/>
+                                </svg>
                             </a>
-                            <a href="{{ $content['social_linkedin'] ?? 'https://www.linkedin.com/company/construction-360' }}" class="hover:text-aqua flex items-center space-x-1.5 transition-colors">
-                                <svg class="h-3.5 w-3.5" fill="currentColor" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.779-1.75-1.75s.784-1.75 1.75-1.75 1.75.779 1.75 1.75-.784 1.75-1.75 1.75zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>
-                                <span>Linkedin</span>
+                            <a href="{{ $content['social_linkedin'] ?? 'https://www.linkedin.com/company/construction-360' }}" target="_blank" aria-label="LinkedIn" class="hover:opacity-80 transition-opacity">
+                                <!-- Original LinkedIn Icon -->
+                                <svg class="h-6 w-6" fill="#0A66C2" viewBox="0 0 24 24">
+                                    <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
+                                </svg>
                             </a>
                         </div>
                     </div>
